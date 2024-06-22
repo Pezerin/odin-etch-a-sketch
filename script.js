b255949041498c826011a4fca6f3149f0db22a1d
Mikul Shah <mikulshah2008@gmail.com>
2024-06-22 15:50:51 -0400

    Added functionality to divs changing background color to black when hovered over

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const div = document.createElement("div");
-        container.appendChild(div);
-    }
+for (let i = 0; i < 16 * 16; i++) {
+    const div = document.createElement("div");
+    container.appendChild(div);
+}
+
+const divs = document.querySelectorAll(".container div");
+
+for (let i = 0; i < 16 * 16; i++) {
+    divs[i].addEventListener("mouseover", () => {
+        divs[i].setAttribute("style", "background-color: black;")
+    });
 }
